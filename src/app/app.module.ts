96aca5cd1875176b86ac92fc014e024b33f7097d
sl0th <ultankearns@gmail.com>
2019-02-04 15:28:06 +0000

    Added styles to homepage + icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'; 
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { SupportComponent } from './support/support.component';
@@ -24,6 +25,7 @@ import { DealsComponent } from './deals/deals.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatIconModule,
     MatButtonModule,
     
   ],
