af08e245c28e69663db0ac7e0603240f31bd565d
Ultan-kearns <ultankearns@gmail.com>
2019-02-19 14:45:48 +0000

    Server showing orders,users,reviews.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'; 
+=======
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon'; 
+import { MatExpansionModule } from '@angular/material/expansion'; 
+>>>>>>> Stashed changes
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { SupportComponent } from './support/support.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { DealsComponent } from './deals/deals.component'; 
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './login/login.component'; 
 import { RetrieveService } from './services/retrieve.service';
 import { AuthenticationService } from './services/authentication.service';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatMenuModule} from '@angular/material/menu';
 import { HttpClientModule } from '@angular/common/http'; 
 import { HttpModule } from '@angular/http';
