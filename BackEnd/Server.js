1bbd08ada8990743ee7f5800d7e093612622047f
ross39 <Rheaney26@gmail.com>
2019-02-20 15:21:39 +0000

    Update Server.js
    Just a blank commit as I don't have python script and routing working properly and don't want to commit broken code

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -89,6 +89,6 @@ app.get('/api/items', function(req, res) {
 //have server listening at port 8081
 var server = app.listen(8081, function() {
   var host = server.address().address
-  var port = server.address().port
+   var port = server.address().port
   console.log("Example app listening at http://%s:%s", host, port)
 })
