75700e5117db72ad7f33095d9ee59fc0e663f277
Ross <Rheaney26@gmail.com>
2019-02-20 17:50:13 +0000

    Got menu routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,17 @@ import { AboutComponent } from './about/about.component';
 import { DealsComponent } from './deals/deals.component';
 import { SupportComponent } from './support/support.component';
 import { LoginComponent } from './login/login.component';
+import  {OutdoorsComponent } from './outdoors/outdoors.component';
+import { SportsComponent} from './sports/sports.component'
+
 const routes: Routes = [
    	{path: 'homepage',component: HomepageComponent},
    	{path: 'about',component: AboutComponent},
 	{path: 'deals',component: DealsComponent},
 	{path: 'support',component: SupportComponent},
 	{path: 'login', component: LoginComponent},
+	{path: 'outdoor', component: OutdoorsComponent},
+	{path: 'sports', component: SportsComponent}
 	];
 
 @NgModule({
