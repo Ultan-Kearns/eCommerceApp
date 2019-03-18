fa15d6d9598266010b69a415e418c4c999952376
Ultan-kearns <ultankearns@gmail.com>
2019-03-18 19:22:17 +0000

    Forgot password fully functional, but have bug if a wrong email is entered it crashes server.

--- src/app/forgot/forgot.component.ts
+++ src/app/forgot/forgot.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ForgotEmailService } from '../services/forgot-email.service'
 import { RetrieveService } from '../services/retrieve.service'
 @Component({
   selector: 'app-forgot',
@@ -8,26 +7,18 @@ import { RetrieveService } from '../services/retrieve.service'
   styleUrls: ['./forgot.component.scss']
 })
 export class ForgotComponent implements OnInit {
-  email:string;
-  constructor(private fs:ForgotEmailService,private rs:RetrieveService) { }
+  constructor(private rs:RetrieveService) { }
   ngOnInit() {
   }
   forgot(form:NgForm)
   {
   	if(form.valid)
   	{
-      if(this.rs.getUserData(this.email) != null)
-      {
-  		alert("Email being sent out now")
-      this.fs.sendMail(this.email);
-      console.log(this.email)
+  		alert("Email being sent out now");
+      this.rs.getUserData(form.value.email).subscribe(data => {
+      console.log(data);
+      alert("Email sent to: " + form.value.email)
+    });
     }
-    else{
-      alert("Account not found");
-    }
-  	}
-  	else{
-  		alert("Please enter email you signed up with")
-  	}
   }
 }
