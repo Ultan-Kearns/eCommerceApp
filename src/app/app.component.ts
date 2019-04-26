d9847f9b5f285d662c696d811c12d01b7f880f16
Ultan-kearns <ultankearns@gmail.com>
2019-04-26 04:46:58 +0100

    Had to delete more files for server and got it running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,23 @@ export class AppComponent implements OnInit {
                 bases[0].setAttribute('href', environment.baseHref);
             }
         }
+    
+    if (!isPlatformBrowser(this.platformId)) {
+        let bases = this.document.getElementsByTagName('base');
+
+        if (bases.length > 0) {
+            bases[0].setAttribute('href', environment.baseHref);
+        }
     }
+
+    if (!isPlatformBrowser(this.platformId)) {
+        let bases = this.document.getElementsByTagName('base');
+
+        if (bases.length > 0) {
+            bases[0].setAttribute('href', environment.baseHref);
+        }
+    }
+}
 
 
   constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any,   private router:Router){
--- src/app/models/bug.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Bug } from './bug';
-
-describe('Bug', () => {
-  it('should create an instance', () => {
-    expect(new Bug()).toBeTruthy();
-  });
-});
